adf2627fde79fef500f6a31f63b63f919af0da96
Welcome <welcomedario9@gmail.com>
2021-10-17 11:19:29 -0600

    added tipos_datos.py

---
+++ tipos_de_datos.py
@@ -0,0 +1,26 @@
+# Una de las caracteristicas fundamentales de python es su simplicidad, esto se
+# traduce en sus tipos de datos: Numéricos, Textos y Booleanos.
+
+# Numéricos: Enteros, Flotantes, Complejos.
+# Texto: Cadenas de texto.
+# Booleanos: true o false.
+
+# Operadores
+# Aritméticos: Suma (+), resta (-), multiplicación (*), división (/), módulo (%)
+# exponente (**), división entera (//).
+# Comparación: Igualdad (==), diferencia (!=), mayor (>), menor (<), mayor o igual (>=)
+# menor o igual (<=).
+# Lógicos: AND, OR, NOT.
+# Asignacion: asignacion (=), incremento suma (+=), decremento resta (-=),
+# incremento multiplicación (*=), división incremental (/=)r.
+# módulo incremental (%=), división entera incremental (//=), exponente incremento (**=).
+# Especiales: IS, IS NOT, IN, NOT IN.
+
+# Una variable es un espacio en la memoria del ordenador donde se almacenará un
+# valor que variará durante la ejecución del programa.
+
+nombre="Darío Welcome"
+print(nombre)
+
+# en python todo es un objeto
+type(nombre)
